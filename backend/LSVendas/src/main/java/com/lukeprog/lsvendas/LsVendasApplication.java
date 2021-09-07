05878a8374d78f62c130d7dea4a8d9d786afb4cc
Lucas Souza <lsilva.2020@alunos.utfpr.edu.br>
2021-09-07 19:46:29 -0300

    Project create

---
+++ backend/LSVendas/src/main/java/com/lukeprog/lsvendas/LsVendasApplication.java
@@ -0,0 +1,13 @@
+package com.lukeprog.lsvendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LsVendasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LsVendasApplication.class, args);
+    }
+
+}
